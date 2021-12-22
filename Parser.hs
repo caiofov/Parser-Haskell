0db70f5f7b6ed68bb868c2175a5592e33e76333b
caiofov <cfoviana@gmail.com>
2021-12-22 02:59:58 -0300

    some upgrades

--- Parser.hs
+++ Parser.hs
@@ -2,10 +2,10 @@ module Parser where
 import Data.Typeable
 
 -- Data types
-data Token = Program | End | Assign | Read | Write | If | While | Begin
+data Token = Program | End | Assign | Read | Write | If | While | Greater | Lesser | GrEqual |LsEqual | Equal | Sum | Sub | Mult | Div | Error
     deriving (Eq, Show)
 
-data TokenTree token = Nil | Node Token (TokenTree token) | Leaf Token | ErrorLeaf String | Head Token String (TokenTree token)
+data TokenTree token = Nil | Node2 Token (TokenTree token) (TokenTree token) | Leaf Token | ErrorLeaf String | Init Token String (TokenTree token) |LeafS String | NodeS String (TokenTree token) | Node1 (TokenTree token)
     deriving (Eq, Show)
 
 cop :: String -> Bool --cop: checks if it's a comparison operator
@@ -16,17 +16,33 @@ top :: String -> Bool --checks if it's a term operator
 top y = y `elem` ["*", "/"]
 
 
-stat :: [Token] -> TokenTree a
-stat (t:s) =if s == [] then Leaf t else Node t (stat s)
-stat _ = ErrorLeaf "Token error"
+stat :: [String] -> TokenTree a
+stat (t:s) =
+        case t of
+            "if" -> Node2 If (stat s) Nil
+            
+            "while" -> Node2 While (stat s) Nil
+            
+            "read" -> Node2 Read (LeafS (s !! 0)) (stat (tail s)) --need to check if t is an indentifier
+            
+            "write" -> Node2 Write (LeafS (s !! 0)) (stat (tail s)) --need to check if t is an indentifier
+            
+            "end" -> Leaf End
+            
+            _ -> if (isIdent t) then 
+                    if (s!!0 == ":=") then Node2 Assign (NodeS t (LeafS (s !! 1 ))) (stat (drop 2 s))-- write expr function
+                    else ErrorLeaf "Invalid assign symbol -> ':=' expected"
+                else ErrorLeaf "Invalid token"
+
+stat _ = ErrorLeaf "Unexpected end of program" --if it doesnt receive a token when it was supposed to
 
 prog :: [String] -> TokenTree a
 prog (h:t) = if (h=="program") then 
-                if (t !! 1 /= ";") then ErrorLeaf " ';' expected"
+                if (t !! 1 /= ";") then ErrorLeaf "';' expected"
                 else
-                    Head Program (t!!0) Nil --insert the other functions here
+                    Init Program (t!!0) (stat (drop 2 t))
             else
-                ErrorLeaf " 'program' expected"
+                ErrorLeaf "'program' expected"
 
 
 fact :: String -> [String] -> [String]
@@ -41,20 +57,56 @@ fact s1 sn =
             else
                 sn
              
+isIdent :: Typeable a => a -> Bool
+isIdent a = (typeOf a == typeOf "String")
 
+input = "program caio ; if read book a := b"
+parser = prog (words input)
 
+checkToken :: String -> Token a --as we can't do the same we do in Oz for haskell (about the record labels), we have this function to return the right token for each input string
+checkToken s = case s of
+    ">" -> Greater
+    ">=" -> GrEqual
+    "<" -> Lesser
+    "<=" -> LsEqual
+    "==" -> Equal
+    "+" -> Sum
+    "*" -> Mult
+    "/" -> Div
+    "-" -> Sub
+    _ -> Error
 
+-- beautify :: TokenTree a -> String
+-- beautify t = (let name = (getTokenName a))
+--             if
+
+-- getTokenName :: TokenTree a -> b
+-- getTokenName (TokenTree name _ _) = name
+-- getTokenName (TokenTree name _) = name
+-- getTokenName (TokenTree name) = name
+
+-- tokenToStr :: Token a -> String
+-- tokenToStr a = case a of
+--     Program -> "Program"
+--     While -> "while"
+--     End -> "end"
+--     Read -> "read"
+--     Write -> "write"
+--     Assign -> "Assign"
+--     If -> "if"
+--     _ -> "?"
 
 -- TO DO
--- Stat 10%
+-- Stat 50%
 -- Sequence
 -- Fact 90%
--- ID
 -- expr, term, comp
 
 -- DONE
--- prog 80%
+-- prog
 -- eop
 -- cop
 -- top
+
+-- ID
 
--- main.hs
+++ main.hs
@@ -3,3 +3,5 @@ import System.IO
 import Parser
 
 main = do --main
+    putStrLn "Write the program to parse"
+    show (prog (words "program caio ; read book"))
