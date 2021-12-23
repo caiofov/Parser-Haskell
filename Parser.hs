a8c1fd95e234c66797000ba28116aece5f558d1c
caiofov <cfoviana@gmail.com>
2021-12-22 22:42:11 -0300

    While token

--- Parser.hs
+++ Parser.hs
@@ -1,12 +1,22 @@
 module Parser where
 import Data.Typeable
-import Data.Either
 
 -- Data types
 data Token = Program | End | Assign | Read | Write | If | While | Greater | Lesser | GrEqual |LsEqual | Equal | Sum | Sub | Mult | Div | Error
     deriving (Eq, Show)
 
-data TokenTree token = Nil | Node2 Token (TokenTree token) (TokenTree token) | Leaf Token | ErrorLeaf String | Init Token String (TokenTree token) |LeafS String | NodeS String (TokenTree token) | Node1 (TokenTree token) | NonParsed String | OpNode Token (String, String)
+data TokenTree token = Nil |    
+                    Node2 Token (TokenTree token) (TokenTree token) | 
+                    Leaf Token | 
+                    ErrorLeaf String | 
+                    Init Token String (TokenTree token) |
+                    LeafS String | 
+                    NodeS String (TokenTree token) | 
+                    Node1 Token (TokenTree token) | 
+                    NonParsed String | 
+                    OpNode Token (TokenTree token, TokenTree token) | 
+                    FuncReturn (TokenTree token) [String] |
+                    Node3 Token (TokenTree token) (TokenTree token) (TokenTree token)
     deriving (Eq, Show)
 
 cop :: String -> Bool --cop: checks if it's a comparison operator
@@ -16,19 +26,40 @@ eop y = y `elem` ["+", "-"]
 top :: String -> Bool --checks if it's a term operator
 top y = y `elem` ["*", "/"]
 
-comp :: [String] -> [String] -> TokenTree a
-comp s1 sn = sequen expr cop s1 sn
-expr :: [String] -> [String] -> TokenTree a
-expr s1 sn = sequen term eop s1 sn
-term :: [String] -> [String] -> TokenTree a
-term s1 sn = sequen fact top s1 sn --fact function doesnt return a tokentree, so we need to create another function to it
+comp :: [String] -> TokenTree a
+comp s1 = sequen expr cop s1
+expr :: [String] -> TokenTree a
+expr s1 = sequen term eop s1
+term :: [String] -> TokenTree a
+term s1 = sequen fact top s1 --fact function doesnt return a tokentree, so we need to create another function to it
 
 stat :: [String] -> TokenTree a
 stat (t:s) =
         case t of
-            "if" -> Node2 If (stat s) Nil
+            "if" -> let c = comp s
+                        c1 = getNonParsedString c 
+                        c2 = returnFunctionToNode c in
+                        if(c1 !! 0 /= "then") then Node2 If c2 (ErrorLeaf "'then' expected")
+                        else
+                            let x = stat (tail c1) 
+                                x1 = getNonParsedString x 
+                                x2 = returnFunctionToNode x in
+                                if(x1 !! 0 /= "else") then Node3 If c2 x2 (ErrorLeaf "'then' expected")
+                                else 
+                                    let z = stat (tail x1) 
+                                        z1 = getNonParsedString z 
+                                        z2 = returnFunctionToNode z in
+                                        Node3 If c2 x2 z2
+                                
             
-            "while" -> Node2 While (stat s) Nil
+            "while" -> 
+                    let c = comp s
+                        c1 = getNonParsedString c 
+                        c2 = returnFunctionToNode c in
+                        if (c1 !! 0 /= "do") then
+                            Node2 While c2 (ErrorLeaf "'do' expected")
+                        else
+                            Node2 While c2 (stat (tail c1))
             
             "read" -> Node2 Read (LeafS (s !! 0)) (stat (tail s)) --need to check if t is an indentifier
             
@@ -51,28 +82,30 @@ prog (h:t) = if (h == "program") then
             else
                 ErrorLeaf "'program' expected"
 
-sequen :: ([String] -> [String] -> TokenTree a) -> (String -> Bool) -> [String] -> [String] -> TokenTree a
-sequen nonterm sep s1 sn = 
-                            if (sep charx1) then do
-                                let t = s1 !! 0
-                                let s3 = tail s1
-                                let x2 = sequen nonterm sep s3 sn
-                                OpNode (checkToken t) (charx1, getNonParsedValue x2)
+sequen :: ([String] -> TokenTree a) -> (String -> Bool) -> [String] -> TokenTree a
+sequen nonterm sep s1 = 
+                            if (sep t) then do
+                                let s3 = tail nonParsedString
+                                let x2 = sequen nonterm sep s3
+                                
+                                let r1 = nonParsedToLeaf x1
+                                let r2 = nonParsedToLeaf x2
+
+                                FuncReturn (OpNode (checkToken t) (r1, r2)) (tail s3)
                             else
-                                NonParsed charx1
-                            where x1 = (nonterm s1 sn) ; charx1 = getNonParsedValue x1
+                                x1
+                            where x1 = (nonterm s1) ; charx1 = getNonParsedValue x1 ; nonParsedString = (getNonParsedString x1); t = nonParsedString !! 0
 
 
-fact :: [String] -> [String] -> TokenTree a
-fact s1 sn = 
+fact :: [String] -> TokenTree a
+fact s1 = 
             if t == "(" then do
-                -- let e = expr (tail sn) sn
+                let e = expr (tail s1)
                 let rest = tail s1
-                -- [e, rest]
-                NonParsed "True"
+                FuncReturn e (splitOn rest ")")
                 
             else
-                NonParsed t
+                FuncReturn (NonParsed t) (tail s1)
             where t = s1!!0
 
 isIdent :: Typeable a => a -> Bool
@@ -92,44 +125,20 @@ checkToken s = case s of
     _ -> Error
 
 getNonParsedValue :: TokenTree a -> String -- as "either" function wasn't working, we need to use only one datatype for the functions returnings.
-getNonParsedValue (NonParsed value) = value
+getNonParsedValue (FuncReturn (NonParsed value) _) = value
 
+getNonParsedString :: TokenTree a -> [String]
+getNonParsedString (FuncReturn _ s) = s
 
--- input = "program caio ; if read book a := b"
--- parser = prog (words input)
+splitOn :: [String] -> String -> [String]
+splitOn (s:t) c = if s==[] then []
+                            else if s == c then t
+                            else splitOn t c
 
+nonParsedToLeaf :: TokenTree a -> TokenTree a
+nonParsedToLeaf a = LeafS (getNonParsedValue a)
+returnFunctionToNode :: TokenTree a -> TokenTree a
+returnFunctionToNode (FuncReturn tree rest) = tree
 
--- beautify :: TokenTree a -> String
--- beautify t = (let name = (getTokenName a))
---             if
-
--- getTokenName :: TokenTree a -> b
--- getTokenName (TokenTree name _ _) = name
--- getTokenName (TokenTree name _) = name
--- getTokenName (TokenTree name) = name
-
--- tokenToStr :: Token a -> String
--- tokenToStr a = case a of
---     Program -> "Program"
---     While -> "while"
---     End -> "end"
---     Read -> "read"
---     Write -> "write"
---     Assign -> "Assign"
---     If -> "if"
---     _ -> "?"
-
--- TO DO
--- Stat 50%
--- Sequence
--- Fact 90%
--- expr, term, comp 98%
-
--- DONE
--- prog
--- eop
--- cop
--- top
-
--- ID
-
+i = prog (words ("program loo ; if x > 5"))
+l = prog (words ("program foo ; while x > 3 do x := 4 end"))
